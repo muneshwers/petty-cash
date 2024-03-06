bbae36146b8b911e803107acf2a0c9e51860a66a
Rajiv Muneshwer <rajivmuneshwer@Rajivs-MacBook-Pro.local>
2024-03-06 10:31:56 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from "express"
+
+const app = express()
+const PORT = process.env.PORT || 3000
+app.set("view engine", "ejs")
+
+app.get("/",(req, res) => {
+  res.render("home", {message: "hello godfrey"})
+})
+
+app.listen(PORT, () => {
+  console.log("App is running...")
+})
