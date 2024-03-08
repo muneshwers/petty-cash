27adeec0d6c9b410f3336aa6fb4a1696770b685d
Rajiv Muneshwer <rajivmuneshwer@Rajivs-MacBook-Pro.local>
2024-03-07 23:47:21 -0400

    transaction style

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 import express from "express"
+import bodyParser from "body-parser"
 
 const app = express()
 const PORT = process.env.PORT || 3000
 app.set("view engine", "ejs")
 app.use("/styles", express.static("styles"))
+app.use(bodyParser.urlencoded({extended :true}))
 
 app.get("/",(req, res) => {
-  res.render("home", {message: "hello godfrey"})
+  res.render("home")
+})
+
+app.get("/login", (req, res) => {
+  const { username, password } = req.body
 })
 
 app.get("/balance", (req, res) => {
