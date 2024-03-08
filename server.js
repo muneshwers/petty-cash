b380363aad7a15cc11a134e501075fe5621a5de2
stephonfrasermuneshwers <162466015+stephonfrasermuneshwers@users.noreply.github.com>
2024-03-08 09:55:28 -0400

    Removed bodyParser, added transactions post and get

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from "express"
-import bodyParser from "body-parser"
+// import bodyParser from "body-parser"
 
 const app = express()
 const PORT = process.env.PORT || 3000
+const transactionLog = []
+let balance = 5000 //Initializes balance with 5000 when server starts
+
 app.set("view engine", "ejs")
 app.use("/styles", express.static("styles"))
-app.use(bodyParser.urlencoded({extended :true}))
+// app.use(bodyParser.urlencoded({extended :true}))
+app.use(express.json());
 
 app.get("/",(req, res) => {
   res.render("home")
@@ -16,8 +20,25 @@ app.get("/login", (req, res) => {
 })
 
 app.get("/balance", (req, res) => {
-  let balance = 5000
   res.json({balance})
+})
+
+//Receives data from form body and pushes to transactionLog array
+app.post("/transactions", async (req, res) => {
+  const data = await req.body;
+  const {recipient, description, amount, date, createdBy } = data;
+  balance = balance - amount;
+  transactionLog.push(data);
+  console.log(transactionLog);
+  console.log("(Server Action) Balance is now $" + balance);
+  console.log("(Server Action) Data Received: "+ recipient + " , " + description  + " , " + amount  + " , " + date + " , " + createdBy);
+})
+
+//Sends the transactionLog to client
+app.get("/transactions", async (req, res) => {
+  res.json({transactionLog})
+  console.log("(Server Action) Returning transactions log")
+  // console.log(transactionLog)
 })
 
 app.listen(PORT, () => {
