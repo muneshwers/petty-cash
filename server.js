448ebee0b981a18a73f7b8e3a476fb726a446c2e
stephonfrasermuneshwers <162466015+stephonfrasermuneshwers@users.noreply.github.com>
2024-03-12 16:30:52 -0400

    Removed duplicates from merge conflict

--- server.js
+++ server.js
@@ -27,35 +27,9 @@ function getUsers() {
   return users
 }
 
-app.set("view engine", "ejs");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/styles", express.static("styles"));
-app.use(expressSession({
-  secret: 'hi123',
-  resave: false,
-  saveUninitialized: false
-}))
-
-let balance = 5000;
-let transactions = [];
-
-function getUsers() {
-  const usersData = fs.readFileSync('./database/users.json');
-  const usersJsonString = usersData.toString('utf-8');
-  const users = JSON.parse(usersJsonString).users; 
-  return users
-}
-
 
 app.get("/balance", (req, res) => {
   res.json({balance})
-})
-
-app.post("/balance", (req ,res) => {
-  const {balance, receipient, description, amount, date} = req.body
-  console.log(req.body)
-  res.render("create_transaction")
 })
 
 
@@ -71,33 +45,10 @@ app.get("/home", (req, res) => {
   }
 })
 
-app.get("/login", (req, res) => {
-  res.render("login", { errorMessage: '' });
-});
-
-app.post("/login/user", (req, res) => {
-  const { username, password } = req.body;
-  console.log(req.body)
-  const users = getUsers();
-
-  let errorMessage = ''; 
-
-  const user = users.find(user => user.username === username && user.password === password);
-
-  if (!user) {
-    errorMessage = "Invalid username or password. Please try again."; 
-    res.render("login", { errorMessage });
-  } else {
-    req.session.loggedIn = true;
-    req.session.username = username;
-    res.render("create_transaction")
-  }
-})
-
 //Sends current balance to homepage
 app.get("/balance", (req, res) => {
   res.json({balance})
-
+})
 
 //Receives form input and updates balance current value
 app.post("/balance", (req ,res) => {
