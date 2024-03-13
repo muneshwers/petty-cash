ad8485d0f6a56c3abf9c14381d8e3e7bb826a277
stephonfrasermuneshwers <162466015+stephonfrasermuneshwers@users.noreply.github.com>
2024-03-13 16:32:20 -0400

    Added func to add data to trans history and remove from curr trans

--- server.js
+++ server.js
@@ -72,6 +72,41 @@ function getTransactionsFile() {
     }
 }
 
+function updateTransactionHistory(transaction) {
+  const existingTransactions = JSON.parse(fs.readFileSync('database/transactionHistory.json'));
+  existingTransactions.transactions.push(transaction);
+  fs.writeFileSync('database/transactionHistory.json', JSON.stringify(existingTransactions));
+}
+
+function deleteFromCurrentTransactions(transactionId) {
+  console.log("(Server Action) Deleting from Current Transactions");
+  const existingTransactions = JSON.parse(fs.readFileSync('database/currentTransactions.json'));
+  console.log("(Server Action) Current Transactions File: ", existingTransactions);
+  let transactionFound = findTransactionById(existingTransactions.transactions, transactionId);
+  console.log("(Server Action) Transaction Found: ", transactionFound);
+
+  if(transactionFound) {
+    console.log("(Server Action) Transaction Found: ", transactionFound);
+    existingTransactions.transactions = removeTransactionById(existingTransactions.transactions, transactionId);
+    console.log("(Server Action) Transaction with ID ", transactionId, ' has been removed.');
+    console.log("(Server Action) Current Transactions now: ", existingTransactions);
+    fs.writeFileSync('database/currentTransactions.json', JSON.stringify(existingTransactions));
+  } else {
+    console.log("(Server Action) Transaction with ID ", transactionId, " not found.");
+    console.log("(Server Action) Aborting Deletion.");
+  }
+  
+}
+
+function findTransactionById(array, id) {
+    return array.find(obj => obj.transactionId === id);
+}
+
+// Function to remove object by id
+function removeTransactionById(array, id) {
+    return array.filter(obj => obj.transactionId !== id);
+}
+
 app.get("/", (req, res) => {
   res.render("home")
 })
@@ -93,8 +128,10 @@ app.get("/balance", (req, res) => {
 
 //Receives form input and updates balance current value
 app.post("/balance", (req ,res) => {
-  const {recipient, description, amount, date,reimburseTotal} = req.body
+  const {recipient, description, amount, date} = req.body
+  console.log("(Server Action) --------------------------- Updating Balance and Current Transactions---------------------------")
   let data = {
+    transactionId:  Math.round(Math.random() * 240),
     recipient,
     description,
     amount,
@@ -104,25 +141,38 @@ app.post("/balance", (req ,res) => {
   updateBalance(amount);
   updateTransactionsFile(data);
 
+  const transactions = getTransactionsFile();
   //Seeing balance updates on server
   const balanceObj = getCurrentBalance();
   const balance = balanceObj.balance
-  const transactions = getTransactionsFile();
   console.log(transactions);
   console.log("(Server Action) Balance is now $" + balance);
-  console.log("(Server Action) Data Received: "+ recipient + " , " + description  + " , " + amount  + " , " + date + " , " + req.session.username);
+  console.log("(Server Action) Data Received: "+ data.transactionId + " , " + recipient + " , " + description  + " , " + amount  + " , " + date + " , " + req.session.username);
   // console.log(req.body)
-  res.render("create_transaction")
+  console.log("(Server Action) ---------------------------[END] Updating Balance and Current Transactions---------------------------")
+  res.render("create_transaction", { currentUser: req.session.username })
 })
 
 app.get("/login", (req, res) => {
   res.render("login", { errorMessage: '' });
 });
 
+//Takes reimbursed total from table and adds to current balance
 app.post("/reimburseBalance",(req,res)=>{
-  const{reimburseTotal} = req.body
-  updateBalancefromReimburse(reimburseTotal)
+  const{reimbursedTotal, toBeReimbursed} = req.body
+  console.log("(Server Action) -------------------- Reimbursing Balance, Adding to History, Removing from Current Transactions---------------------------")
+  console.log('Received: ', reimbursedTotal)
+  console.log('Data to send to history: ', toBeReimbursed)
+  updateBalancefromReimburse(reimbursedTotal)
+  toBeReimbursed.map( transaction => {
+    updateTransactionHistory(transaction)
+    deleteFromCurrentTransactions(transaction.transactionId)
+    console.log('Sent: ', transaction);
+  })
+  console.log("(Server Action) --------------------[END] Reimbursing Balance, Adding to History, Removing from Current Transactions---------------------------")
+  res.render("reimburse")
 })
+
 app.post("/login/user", (req, res) => {
   const { username, password } = req.body;
   console.log(req.body)
@@ -140,6 +190,10 @@ app.post("/login/user", (req, res) => {
     req.session.username = username;
     res.render("create_transaction")
   }
+})
+
+app.get("/create_transaction", (req, res) => {
+  res.render("create_transaction")
 })
 
 app.get("/reimburse", (req, res) => {
